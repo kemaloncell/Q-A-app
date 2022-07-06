647c2b4bf6b7f47ded834e5b81f9cd24dce0411d
kemaloncell <57701397+kemaloncell@users.noreply.github.com>
2022-07-06 15:11:09 +0300

    Quiz Constructor

--- script.js
+++ script.js
@@ -11,24 +11,47 @@
     }
 } */
 
+// Question Class
 function Question(questionText, options, trueAnswer) {
     this.questionText = questionText;
     this.options = options;
     this.trueAnswer = trueAnswer;
-    console.log(this)
 }
 //We've stored the function there using the prototype property of the Question object
 // so that it doesn't repeat the function inside the Question Object every time.
 Question.prototype.checkAnswer = function (answer) {
     return answer === this.trueAnswer;
 }
-let question1 = new Question("Which is javascript package management?",{a: "TypeScript", b: "Node.js", c: "npm"}, "c");
-let question2 = new Question("Which is .net package management?",{a: "nuget", b: "Node.js", c: "npm"}, "a");
+
 
 let questions=[
-    new Question("Which is javascript package management?",{a: "TypeScript", b: "Node.js", c: "npm"}, "c"),
-    new Question("Which is .net package management?",{a: "nuget", b: "Node.js", c: "npm"}, "a"),
-    new Question("Which is javascript package management?",{a: "TypeScript", b: "Node.js", c: "npm"}, "c"),
-    new Question("Which is javascript package management?",{a: "TypeScript", b: "Node.js", c: "npm"}, "c"),
+    new Question("1-Which is javascript package management?",{a: "TypeScript", b: "Node.js", c: "npm"}, "c"),
+    new Question("2-Which is .net package management?",{a: "nuget", b: "Node.js", c: "npm"}, "a"),
+    new Question("3-Which is javascript package management?",{a: "TypeScript", b: "Node.js", c: "npm"}, "c"),
+    new Question("4-Which is javascript package management?",{a: "TypeScript", b: "Node.js", c: "npm"}, "c"),
 
 ]
+
+// Quiz Class
+function Quiz(questions) {
+    this.questions = questions;
+    this.questionIndex = 0;
+}
+
+//Add getQuestion function on  Quiz prototype
+Quiz.prototype.getQuestion = function () {
+    return this.questions[this.questionIndex]
+}
+
+const quiz = new Quiz(questions);
+
+document.querySelector(".btn-start").addEventListener("click", function (){
+    if(quiz.questions.length !== quiz.questionIndex){
+        console.log(quiz.getQuestion())
+        quiz.questionIndex += 1;
+    }else{
+        console.log("Quiz Over")
+    }
+
+});
+
