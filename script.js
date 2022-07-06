6bc3ffc30dba0f872d82b222a3d48f0dcba2d37c
kemaloncell <57701397+kemaloncell@users.noreply.github.com>
2022-07-06 14:23:18 +0300

    preparing questions by creating a constructor

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let question = {
+/*let question = {
     questionText: "Which is javascript package management?",
     options: {
         a: "TypeScript",
@@ -9,7 +9,36 @@ let question = {
     checkAnswer: function (answer) {
       return answer === this.trueAnswer;
     }
+} */
+
+function Question(questionText, options, trueAnswer) {
+    this.questionText = questionText;
+    this.options = options;
+    this.trueAnswer = trueAnswer;
+    this.checkAnswer = function (answer) {
+        return answer === this.trueAnswer;
+    }
 }
 
-console.log(question.questionText)
-console.log(question.checkAnswer("a"))
+let question1 = new Question("Which is javascript package management?",{a: "TypeScript", b: "Node.js", c: "npm"}, "c");
+let question2 = new Question("Which is .net package management?",{a: "nuget", b: "Node.js", c: "npm"}, "a");
+
+let questions=[
+    new Question("Which is javascript package management?",{a: "TypeScript", b: "Node.js", c: "npm"}, "c"),
+    new Question("Which is .net package management?",{a: "nuget", b: "Node.js", c: "npm"}, "a"),
+    new Question("Which is javascript package management?",{a: "TypeScript", b: "Node.js", c: "npm"}, "c"),
+    new Question("Which is javascript package management?",{a: "TypeScript", b: "Node.js", c: "npm"}, "c"),
+
+]
+
+for(let s in questions) {
+    console.log(questions[s].questionText);
+    console.log(questions[s].trueAnswer);
+    console.log(questions[s].checkAnswer("c"));
+}
+
+
+console.log(question1.questionText);
+console.log(question1.trueAnswer);
+console.log(question1.checkAnswer("c"));
+
