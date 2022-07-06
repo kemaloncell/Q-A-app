cc762e2aaa6c9450da13473381bde9a6925085f8
kemaloncell <57701397+kemaloncell@users.noreply.github.com>
2022-07-06 19:43:59 +0300

    show question on card

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ Question.prototype.checkAnswer = function (answer) {
 
 
 let questions=[
-    new Question("1-Which is javascript package management?",{a: "TypeScript", b: "Node.js", c: "npm"}, "c"),
+    new Question("1-Which is javascript package management?",{a: "TypeScript", b: "Node.js", c: "Npm", d:"Nuget"}, "c"),
     new Question("2-Which is .net package management?",{a: "nuget", b: "Node.js", c: "npm"}, "a"),
     new Question("3-Which is javascript package management?",{a: "TypeScript", b: "Node.js", c: "npm"}, "c"),
     new Question("4-Which is javascript package management?",{a: "TypeScript", b: "Node.js", c: "npm"}, "c"),
@@ -48,11 +48,32 @@ const quiz = new Quiz(questions);
 document.querySelector(".btn-start").addEventListener("click", function (){
     if(quiz.questions.length !== quiz.questionIndex){
         document.querySelector(".quiz-box").classList.add("active")
-        console.log(quiz.getQuestion())
-        quiz.questionIndex += 1;
+        showQuestion(quiz.getQuestion());
     }else{
         console.log("Quiz Over")
     }
 
 });
 
+document.querySelector(".btn_next").addEventListener("click", function (){
+    if(quiz.questions.length !== quiz.questionIndex +1){
+        document.querySelector(".quiz-box").classList.add("active")
+        quiz.questionIndex += 1;
+        showQuestion(quiz.getQuestion());
+    }else{
+        console.log("Quiz Over")
+    }
+})
+function showQuestion(question){
+    let questionText = `<span>${question.questionText}</span>`;
+    let options ='';
+
+    for(let answer in question.options){
+        options += `<div class="option">
+        <span><b>${answer}</b>: ${question.options[answer]}</span>
+        </div>`;
+    }
+
+    document.querySelector(".question-text").innerHTML = questionText;
+    document.querySelector(".option_list").innerHTML = options;
+}
