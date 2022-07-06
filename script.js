6d2d7f24f3cef2455df6133a2d1b75218dc5d667
kemaloncell <57701397+kemaloncell@users.noreply.github.com>
2022-07-06 16:22:23 +0300

    crated quiz card

--- script.js
+++ script.js
@@ -47,6 +47,7 @@ const quiz = new Quiz(questions);
 
 document.querySelector(".btn-start").addEventListener("click", function (){
     if(quiz.questions.length !== quiz.questionIndex){
+        document.querySelector(".quiz-box").classList.add("active")
         console.log(quiz.getQuestion())
         quiz.questionIndex += 1;
     }else{
